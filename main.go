7bdfe9ec261f1cf066bbdb47132f3e24d3c11aa6
krl42c <karolc9991@gmail.com>
2024-03-23 13:10:46 +0100

    handle sigwinch

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/krl42c/healthyy/internal/parser"
@@ -26,6 +28,15 @@ func main() {
 	}
 
 	fmt.Print("\033[H\033[2J")
+
+	// handle terminal resizing
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGWINCH)
+	go func() {
+		for range c {
+			fmt.Print("\033[H\033[2J")
+		}
+	}()
 
 	for index, entry := range parsedConf {
 		go monitor(entry, index)
